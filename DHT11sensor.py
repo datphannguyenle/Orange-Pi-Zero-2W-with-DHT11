24024d69becbe8fec9a9f319c4cda004007e5539
Dat Phan Nguyen Le <82134525+datphannguyenle@users.noreply.github.com>
2024-09-27 18:33:22 +0700

    Update DHT11sensor.py

--- DHT11sensor.py
+++ DHT11sensor.py
@@ -1,69 +1,78 @@
 import wiringpi
 from wiringpi import GPIO
 
-pin = 6
-
-def getval(pin):
-    tl = []
-    tb = []
-    wiringpi.wiringPiSetup()
-    wiringpi.pinMode(pin, GPIO.OUTPUT)
-    wiringpi.digitalWrite(pin, GPIO.HIGH)
-    wiringpi.delay(1)
-    wiringpi.digitalWrite(pin, GPIO.LOW)
-    wiringpi.delay(25)
-    wiringpi.digitalWrite(pin, GPIO.HIGH)
-    wiringpi.delayMicroseconds(20)
-    wiringpi.pinMode(pin, GPIO.INPUT)
-    while(wiringpi.digitalRead(pin) == 1): 
-        pass
+class DHT11:
+    def __init__(self, pin):
+        self.pin = pin
+        wiringpi.wiringPiSetup()
     
-    for i in range(45):
-        tc = wiringpi.micros()
-        while(wiringpi.digitalRead(pin) == 0): 
+    def getval(self):
+        tl = []
+        tb = []
+        # Set pin mode to OUTPUT and send start signal
+        wiringpi.pinMode(self.pin, GPIO.OUTPUT)
+        wiringpi.digitalWrite(self.pin, GPIO.HIGH)
+        wiringpi.delay(1)
+        wiringpi.digitalWrite(self.pin, GPIO.LOW)
+        wiringpi.delay(25)
+        wiringpi.digitalWrite(self.pin, GPIO.HIGH)
+        wiringpi.delayMicroseconds(20)
+        wiringpi.pinMode(self.pin, GPIO.INPUT)
+        
+        # Wait for DHT11 response
+        while wiringpi.digitalRead(self.pin) == 1:
             pass
-        while(wiringpi.digitalRead(pin) == 1):
+        
+        # Read data from DHT11
+        for i in range(45):
+            tc = wiringpi.micros()
+            while wiringpi.digitalRead(self.pin) == 0:
+                pass
+            while wiringpi.digitalRead(self.pin) == 1:
+                if wiringpi.micros() - tc > 500:
+                    break
             if wiringpi.micros() - tc > 500:
                 break
-        if wiringpi.micros() - tc > 500:
-            break
-        tl.append(wiringpi.micros() - tc)
-
-    tl = tl[1:]
-    for i in tl:
-        if i > 100:
-            tb.append(1)
-        else:
-            tb.append(0)
-    
-    return tb
-
-def GetResult(pin):
-    for i in range(10):
-        SH = 0
-        SL = 0
-        TH = 0
-        TL = 0
-        C = 0
-        result = getval(pin)
-
-        if len(result) == 40:
-            for i in range(8):
-                SH *= 2; SH += result[i]    # humi Integer
-                SL *= 2; SL += result[i+8]  # humi decimal
-                TH *= 2; TH += result[i+16] # temp Integer
-                TL *= 2; TL += result[i+24] # temp decimal
-                C *= 2; C += result[i+32]   # Checksum
-            if ((SH + SL + TH + TL) % 256) == C and C != 0:
-                break
+            tl.append(wiringpi.micros() - tc)
+        
+        # Process the data
+        tl = tl[1:]
+        for i in tl:
+            if i > 100:
+                tb.append(1)
             else:
-                print("Read Success, But checksum error! retrying")
+                tb.append(0)
+        
+        return tb
 
-        else:
-            print("Read failer! Retrying")
-            break
-        wiringpi.delay(200)
-    return SH, SL, TH, TL
+    def get_result(self):
+        for _ in range(10):
+            SH = 0
+            SL = 0
+            TH = 0
+            TL = 0
+            C = 0
+            result = self.getval()
 
-SH, SL, TH, TL = GetResult(pin)
+            if len(result) == 40:
+                for i in range(8):
+                    SH *= 2; SH += result[i]    # humi Integer
+                    SL *= 2; SL += result[i+8]  # humi decimal
+                    TH *= 2; TH += result[i+16] # temp Integer
+                    TL *= 2; TL += result[i+24] # temp decimal
+                    C *= 2; C += result[i+32]   # Checksum
+                if ((SH + SL + TH + TL) % 256) == C and C != 0:
+                    break
+                else:
+                    print("Read Success, But checksum error! retrying")
+
+            else:
+                print("Read failer! Retrying")
+                break
+            wiringpi.delay(200)
+        return SH, SL, TH, TL
+
+# Example usage
+dht11_sensor = DHT11(pin=6)
+SH, SL, TH, TL = dht11_sensor.get_result()
 print("humi:", SH, SL, "temp:", TH, TL)
